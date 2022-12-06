bec5e66e70d8d4fdccdf5597c2d6a950b8ec9e17
ParkRootSeok <g_e_un@naver.com>
2022-12-06 16:15:49 +0900

    feat : Create Kernel

--- CNN/cnn_opencl.c
+++ CNN/cnn_opencl.c
@@ -11,12 +11,33 @@
         exit(EXIT_FAILURE); \
     }  //에러 확인 함수
 
+void build_error(cl_program program, cl_device_id device, cl_int err)
+{
+	if (err == CL_BUILD_PROGRAM_FAILURE)
+	{
+		size_t log_size;
+		char* log;
+
+		err = clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, 0, (void*)0, &log_size);
+		CHECK_ERROR(err);
+
+		log = (char*)malloc(log_size + 1);
+		err = clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, log_size, log, (void*)0);
+		CHECK_ERROR(err);
+
+		log[log_size] = '\0';
+		printf("Compiler error:\n%s\n", log);
+		free(log);
+		exit(0);
+	};
+}
+
 char* get_source_code(const char* file_name, size_t* len) {
 	char* source_code;
 	char buf[2] = "\0";
 	int cnt = 0;
 	size_t length;
-	FILE* file = fopen("kernel.cl", "r");
+	FILE* file = fopen(file_name, "r");
 	if (file == NULL) {
 		printf("[%s:%d] Failed to open %s\n", __FILE__, __LINE__, file_name);
 		exit(EXIT_FAILURE);
@@ -47,19 +68,27 @@ char* get_source_code(const char* file_name, size_t* len) {
 cl_int err;					// Variable for Error check 
 cl_platform_id platform;	// Platform ID
 cl_device_id device;		// Device ID
+cl_context context;
+cl_program program;
+cl_kernel convolution_layer, pooling_layer, fc_layer;
 
-void cnn_init() {
+void cnn_init(void) {
 	
+	/* GET DEVICE INFO */
 	err = clGetPlatformIDs(1, &platform, NULL);
 	CHECK_ERROR(err);
 
 	err = clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 1, &device, NULL);
 	CHECK_ERROR(err);
 
-	/* Create a context */
-	cl_context context;
+	/* CREATE CONTEXT */
 	context = clCreateContext(NULL, 1, &device, NULL, NULL, &err);
 	CHECK_ERROR(err);
+
+	/* CREATE KERNEL*/
+	convolution_layer = creat_kernel("convolution_layer");
+	pooling_layer = creat_kernel("pooling_layer");
+	fc_layer = creat_kernel("fc_layer");
 
 }
 
@@ -71,4 +100,46 @@ void cnn(float* images, float** network, int* labels, float* confidences, int nu
 	 * See "cnn_seq.c" if you don't know what to do.
 	 */
 
+
+
+
+	
+}
+
+cl_command_queue* creat_queue(int count)
+{
+	cl_int err = 0;
+	cl_command_queue* queue = (cl_command_queue*)malloc(sizeof(cl_command_queue) * count);
+	for (int i = 0; i < count; i++) {
+		queue[i] = clCreateCommandQueue(context, device, 0, &err);
+		CHECK_ERROR(err);
+	}
+
+	return queue;
+}
+
+void creat_program(void)
+{
+	cl_int err;
+	size_t source_code_len;
+	char* source_code = get_source_code("kernel.cl", &source_code_len);
+	program = clCreateProgramWithSource(context, 1, (const char**)&source_code, &source_code_len, &err);
+	CHECK_ERROR(err);
+}
+
+void build_program(void)
+{
+	cl_int err;
+	err = clBuildProgram(program, 1, &device, "", NULL, NULL);
+	build_error(program, device, err);
+	CHECK_ERROR(err);
+}
+
+cl_kernel creat_kernel(const char* kernel_name)
+{
+	cl_int err;
+	cl_kernel kernel = clCreateKernel(program, kernel_name, &err);
+	CHECK_ERROR(err);
+
+	return kernel;
 }
