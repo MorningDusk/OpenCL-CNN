6b37ad5f0c9dfa53e568ac33918486654c17b04d
ParkRootSeok <g_e_un@naver.com>
2022-12-07 05:45:57 +0900

    Conflict 해결 및 Test Code 작성

--- CNN/main.c
+++ CNN/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include "cnn.h"
+#include "compare.h"
 
 const char* CLASS_NAME[] = {
 	"airplane",
@@ -75,6 +76,7 @@ int* read_labels(size_t n) {
  * fc3     : weight ( 10, 512) bias ( 10)
  * Thus, 60980520 bytes are expected.
  */
+
 const int NETWORK_SIZES[] = {
 	64 * 3 * 3 * 3, 64,
 	64 * 64 * 3 * 3, 64,
